533fe79745f15abea2b6486330b62efd0871e65e
Navadeep Jabu <navadeepjabu@Navadeeps-MacBook-Air.local>
2026-02-12 14:46:11 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 
 
 app.use("/api/auth", authRoutes);
